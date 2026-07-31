a2d8063e327e30fa94dbc3038cecdb38e1c15faa
Hooyeye <121609295+Hooyeye@users.noreply.github.com>
2026-07-30 18:37:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname)));
 
 function cleanText(value, max = 10000) {
   return String(value || "").trim().slice(0, max);
@@ -412,7 +412,7 @@ app.use("/api", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.use((error, req, res, next) => {
